cb9dd81dac0ae297f3795066309e6460c097f762
Elijah Smith <elijahsmith.m@gmail.com>
2022-08-04 19:16:27 -0500

    commit tree 1de4df48707eb2fc1448e223025947ccf42d4c4a

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  const catsCopy = [...cats];
+  catsCopy.splice(2, 1);
+  return catsCopy;
+}
+
+function removeFirstCat() {
+  const catsCopy = [...cats];
+  catsCopy.splice(0, 1);
+  return catsCopy;
+}
